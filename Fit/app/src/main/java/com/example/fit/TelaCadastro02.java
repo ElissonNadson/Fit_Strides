a5d959dfa91010b1cd8d895a8abb6d31f77b653c
ElissonNadson <nadsonnodachi@gmail.com>
2023-12-13 02:39:14 -0300

    foto e cadastro de perfil

--- Fit/app/src/main/java/com/example/fit/TelaCadastro02.java
+++ Fit/app/src/main/java/com/example/fit/TelaCadastro02.java
@@ -14,67 +14,82 @@ public class TelaCadastro02 extends AppCompatActivity {
 
     private EditText ageInput, heightInput, weightInput;
     private Button btnContinuar;
-
-    private ImageButton btnVoltar;
+    private ImageButton btnVoltar, ageIncrease, ageDecrease, heightIncrease, heightDecrease, weightIncrease, weightDecrease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro02);
 
+        initComponents();
+
+        btnVoltar.setOnClickListener(v -> finish());
+
+        ageIncrease.setOnClickListener(v -> adjustInputByOne(ageInput, true));
+        ageDecrease.setOnClickListener(v -> adjustInputByOne(ageInput, false));
+        heightIncrease.setOnClickListener(v -> adjustInputByOne(heightInput, true));
+        heightDecrease.setOnClickListener(v -> adjustInputByOne(heightInput, false));
+        weightIncrease.setOnClickListener(v -> adjustInputByOne(weightInput, true));
+        weightDecrease.setOnClickListener(v -> adjustInputByOne(weightInput, false));
+
+        btnContinuar.setOnClickListener(v -> goToTelaCadastro03());
+    }
+
+    private void initComponents() {
+        // Conectar inputs e botões com os ids do layout
         ageInput = findViewById(R.id.age_input);
         heightInput = findViewById(R.id.height_input);
         weightInput = findViewById(R.id.weight_input);
         btnVoltar = findViewById(R.id.bt_voltar);
         btnContinuar = findViewById(R.id.continue_button);
 
-        btnVoltar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-
-        ImageButton ageIncrease = findViewById(R.id.age_increase);
-        ImageButton ageDecrease = findViewById(R.id.age_decrease);
-
-        // Defina os listeners para ageIncrease e ageDecrease
-        // ...
-
-        btnContinuar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                irParaTelaCadastro03();
-            }
-        });
+        ageIncrease = findViewById(R.id.age_increase);
+        ageDecrease = findViewById(R.id.age_decrease);
+        heightIncrease = findViewById(R.id.height_increase);
+        heightDecrease = findViewById(R.id.height_decrease);
+        weightIncrease = findViewById(R.id.weight_increase);
+        weightDecrease = findViewById(R.id.weight_decrease);
     }
 
-    private void irParaTelaCadastro03() {
-        String age = ageInput.getText().toString();
-        String height = heightInput.getText().toString();
-        String weight = weightInput.getText().toString();
-
-        if (age.isEmpty()) {
-            Toast.makeText(TelaCadastro02.this, "Informe sua idade.", Toast.LENGTH_SHORT).show();
-            return;
+    private void adjustInputByOne(EditText input, boolean increase) {
+        int currentValue = 0;
+        if (!input.getText().toString().isEmpty()) {
+            currentValue = Integer.parseInt(input.getText().toString());
         }
-        if (height.isEmpty()) {
-            Toast.makeText(TelaCadastro02.this, "Informe sua altura.", Toast.LENGTH_SHORT).show();
-            return;
+        if (increase) {
+            currentValue++;
+        } else {
+            currentValue = (currentValue > 0) ? currentValue - 1 : 0;
         }
-        if (weight.isEmpty()) {
-            Toast.makeText(TelaCadastro02.this, "Informe seu peso.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        Intent intent = new Intent(TelaCadastro02.this, TelaCadastro03.class);
-        intent.putExtras(getIntent().getExtras()); // Inclui as informações anteriores
-        intent.putExtra("IDADE", age);
-        intent.putExtra("ALTURA", height);
-        intent.putExtra("PESO", weight);
-        startActivity(intent);
+        input.setText(String.valueOf(currentValue));
     }
 
-    // Implemente os métodos para incrementar e decrementar a idade, altura e peso
-    // ...
+    private void goToTelaCadastro03() {
+        if (validateInputs()) {
+            String age = ageInput.getText().toString();
+            String height = heightInput.getText().toString();
+            String weight = weightInput.getText().toString();
+
+            Intent intent = new Intent(TelaCadastro02.this, TelaCadastro03.class);
+            intent.putExtras(getIntent().getExtras()); // Passando os dados recebidos
+            intent.putExtra("IDADE", age);
+            intent.putExtra("ALTURA", height);
+            intent.putExtra("PESO", weight);
+            startActivity(intent);
+        }
+    }
+
+    private boolean validateInputs() {
+        if (ageInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe sua idade.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (heightInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe sua altura.", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (weightInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Informe seu peso.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
 }
--- Fit/app/src/main/java/com/example/fit/TelaCadastro03.java
+++ Fit/app/src/main/java/com/example/fit/TelaCadastro03.java
@@ -1,36 +1,151 @@
 package com.example.fit;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.widget.Toast;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+// Um exemplo simples da classe Usuario que você deve definir em outro arquivo:
+class Usuario {
+    public String nome, email, senha, genero, idade, altura, peso, imageUrl;
+
+    public Usuario(String nome, String email, String senha, String genero, String idade, String altura, String peso, String imageUrl) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.genero = genero;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+        this.imageUrl = imageUrl;
+    }
+
+    // Métodos getters e setters se necessários
+}
 
 public class TelaCadastro03 extends AppCompatActivity {
 
-    // Incluir elementos para seleção de fotos
+    private static final int PICK_IMAGE_REQUEST = 123;
+    private ImageView imagePerfil;
+    private Button botaoFinalizarCadastro;
+    private Uri imagemUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_cadastro03);
 
-        // Implementar captura ou seleção de fotos e salvar no Firebase
-        // ...
+        imagePerfil = findViewById(R.id.image_perfil);
+        botaoFinalizarCadastro = findViewById(R.id.botao_finalizar_cadastro);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            String nome = extras.getString("NOME");
-            String email = extras.getString("EMAIL");
-            String senha = extras.getString("SENHA");
-            String genero = extras.getString("GENERO");
-            String idade = extras.getString("IDADE");
-            String altura = extras.getString("ALTURA");
-            String peso = extras.getString("PESO");
-            // Incluir lógica para capturar a foto e salvar todos os dados no Firebase
+        imagePerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrirGaleria();
+            }
+        });
+
+        botaoFinalizarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle extras = getIntent().getExtras();
+                if (extras != null) {
+                    if (imagemUri != null) {
+                        // Se a imagem foi selecionada, inicie o upload e depois salve os dados do usuário
+                        uploadImageAndSaveUserData(extras);
+                    } else {
+                        // Se nenhuma imagem foi selecionada, apenas salve os dados do usuário sem uma URL de imagem
+                        saveUserData(extras, null);
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            imagemUri = data.getData();
+            imagePerfil.setImageURI(imagemUri);
         }
     }
 
-    // Precisará de um método para salvar os dados no Firebase e fazer upload das fotos para o Firebase Storage
-    // ...
+    private void abrirGaleria() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Selecione a Imagem"), PICK_IMAGE_REQUEST);
+    }
+
+    private void uploadImageAndSaveUserData(final Bundle extras) {
+        final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("profileImages").child(userId + ".jpg");
+
+        fileRef.putFile(imagemUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        String imageUrl = uri.toString();
+                        saveUserData(extras, imageUrl);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(TelaCadastro03.this, "Falha no upload", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void saveUserData(Bundle extras, String imageUrl) {
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        Usuario usuario = new Usuario(
+                extras.getString("NOME"),
+                extras.getString("EMAIL"),
+                extras.getString("SENHA"),
+                extras.getString("GENERO"),
+                extras.getString("IDADE"),
+                extras.getString("ALTURA"),
+                extras.getString("PESO"),
+                imageUrl
+        );
+
+        FirebaseDatabase.getInstance().getReference()
+                .child("Users")
+                .child(userId)
+                .setValue(usuario)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Redireciona para a tela de login após cadastro
+                        Intent intent = new Intent(TelaCadastro03.this, FormLogin.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(TelaCadastro03.this, "Falha ao salvar os dados", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
